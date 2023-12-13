6385be389f527319d5f10f3591fd9515799b870f
Patsagorn <patsagorn_y@outlook.com>
2023-12-13 14:51:11 +0700

    Add enhanced-img package and configure Vite plugins

---
+++ src/lib/components/ShowLab.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { ClickableTile } from 'carbon-components-svelte';
+	export let href = '';
+</script>
+
+<ClickableTile {href} class="flex align-top">
+	<slot />
+	<code class="px-2 mt-3 text-sm"> ⬢{href}</code>
+</ClickableTile>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,43 @@
 <script lang="ts">
-	import { UnorderedList } from 'carbon-components-svelte';
-	import LabLink from '$lib/components/LabLink.svelte';
+	import { ClickableTile, Column, Grid, Row, UnorderedList } from 'carbon-components-svelte';
+	import ShowLab from '$lib/components/ShowLab.svelte';
 </script>
 
-<h1>Patsagorn's Code Labs</h1>
-<p>
-	Welcome to my playground! In here will be mostly my mini project or code that I interested in for
-	Web devlopment.
-</p>
-<h2>Lists</h2>
-<UnorderedList>
-	<LabLink href="dotted-hover-background">Dotted Hover Background</LabLink>
-	<LabLink href="sync-todo">Sync Todo</LabLink>
-	<LabLink href="rock-paper-scissors-tournament">Rock paper scissors tournament</LabLink>
-</UnorderedList>
+<svelte:head>
+	<title>Patsagorn's Code Labs</title>
+	<meta
+		name="description"
+		content="Welcome to my playground! In here will be mostly my mini project or code that I interested in for Web devlopment."
+	/>
+</svelte:head>
+
+<div class="flex flex-col">
+	<div class="">
+		<h1>Patsagorn's Code Labs</h1>
+		<p>
+			Welcome to my playground! In here will be mostly my mini project or code that I interested in
+			for Web devlopment.
+		</p>
+	</div>
+	<div class="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+		<ShowLab href="/dotted-hover-background">
+			<enhanced:img
+				class="grid-image"
+				src="$lib/thumbnail/dotted-hover-background.png"
+				title="Dotted Hover Background"
+			/>
+		</ShowLab>
+		<ShowLab href="/sync-todo">
+			<enhanced:img class="grid-image" src="$lib/thumbnail/sync-todo.png" />
+		</ShowLab>
+		<ShowLab href="/rock-paper-scissors-tournament">
+			<enhanced:img class="grid-image" src="$lib/thumbnail/rock-paper-scissors-tournament.png" />
+		</ShowLab>
+	</div>
+</div>
+
+<style lang="scss">
+	.grid-image {
+		@apply aspect-video object-cover align-bottom justify-start;
+	}
+</style>
